3210d969c3fb662a361704624bce5e265f6bf5ef
Viktor Kirillov <mr.kirillov@icloud.com>
2019-06-29 16:16:05 -0500

    Youtube search returns desired wildbook format

--- viktor/classes/YouTube.py
+++ viktor/classes/YouTube.py
@@ -17,72 +17,99 @@ class YouTube:
         self.db = db
     
     # Uses youtube.search() API method
-    def search(self, q, limit=1, fields=False, details=False, links=False, videoOnly=False, save=False):
+    def search(self, q, limit=1, fields=False, save=False):
         ''' 
         q - search query, (ex. "Whale Shark")
         limit - number of results (ex. 10 or 100)
         fields - Retrieve only selected fields (ex. True, False)
-        details - Retrieves more detailed information about the video (ex. True, False)
-                  Cannot be True when videoOnly=False.
-        links - Generate YouTube video links for each search result. 
-                Can be found in video['url'] (ex. True, False) 
-        videosOnly - Return only videos from the result (ex. True, False)
         save - Saves all retrieved videos to database (ex. True, False)
         '''
-        if (details and not videoOnly):
-            print('You cannot use details=True and videoOnly=False together. But any other combination is valid.')
-            return
-        
-        if (save):
-            if (not self.db):
-                save = False
-                print("Please provide 'db' argument with an instance to database to save video(s).")
+        if (save and not self.db):
+            save = False
+            print("Please provide 'db' argument with an instance to database to save video(s).")
         
         # Quering the result
         searchResult = self.youtube.search().list(
             q=q,
             part='snippet',
             fields='items(id,snippet(publishedAt,channelId,title,description))' if fields else '*',
-            type='video' if videoOnly else 'video,channel,playlist',
+            type='video',
             maxResults=limit
         ).execute()
+
+        # Saving original YouTube responce for last search
         self.lastSearch = searchResult['items']
                 
-        
-        # Handling additional parameters
-        if (details or save or links):
-            for item in self.lastSearch:
-                
-                # Quering more details for this result
-                if (details):
-                    details = self.videos(item['id']['videoId'], fields=True)
-                    for prop in details[0]:
-                        try:
-                            item[prop].update(details[0][prop])
-                        except KeyError:
-                            item[prop] = details[0][prop]
-                            
-                # Generating YouTube video links
-                if (links):
-                    if (item['id']['kind'] == 'youtube#video'):
-                        item['url'] = 'https://youtu.be/' + item['id']['videoId']
-                    elif (item['id']['kind'] == 'youtube#channel'):
-                        item['url'] = 'https://www.youtube.com/channel/' + item['id']['channelId']
+        modifiedResult = []
 
-                # Saving item in database
-                if (save):
-                    self.db.addVideo(item['id']['videoId'], item)
-                    
+        # Going through each result
+        for item in self.lastSearch:
+
+            # Quering more details for this result
+            details = self.videos(item['id']['videoId'], fields=True)
+            for prop in details[0]:
+                try:
+                    item[prop].update(details[0][prop])
+                except KeyError:
+                    item[prop] = details[0][prop]
+
+            # WILDBOOK FORMAT
+            newItem = {
+                "videoID": item['id']['videoId'],
+                "title": {
+                    "original": item['snippet']['title'],
+                    "eng": item['snippet']['title'], #[Microsoft translate]
+                },
+                "tags": {
+                    "original": item['snippet']['tags'],
+                    "eng": item['snippet']['tags'], #[Microsoft translate]
+                },
+                "description": {
+                    "original": item['snippet']['description'],
+                    "eng": item['snippet']['description'], #[Microsoft translate]
+                },
+                "OCR": {
+                    "original": [], #[Azure]
+                    "eng": [], #[Azure, Microsoft translate]
+                },
+                "url": 'https://youtu.be/' + item['id']['videoId'],
+                "animalsID": [], #[Wildbook]
+                "curationStatus": None, #[Wildbook]
+                "curationDecision": None, #[Wildbook]
+                "publishedAt": item['snippet']['publishedAt'],
+                "uploadedAt": None, #[YouTube]
+                "duration": None, #[YouTube]
+                "regionRestriction": None, #[YouTube]
+                "viewCount": item['statistics']['viewCount'],
+                "likeCount": item['statistics']['likeCount'],
+                "dislikeCount": item['statistics']['dislikeCount'],
+                "recordingDetails": {
+                    "location": None, #[YouTube]
+                    "date": None, #[YouTube]
+                },
+                "encounter": {
+                    "locationIDs": [], #[Wildbook]
+                    "dates": [], #[Wildbook]
+                },
+                "fileDetails": None, #[YouTube]
+            }
+
+            # Saving item in database
+            if (save):
+                self.db.addVideo(item['id']['videoId'], newItem)
+
+            modifiedResult.append(newItem)
 
         # Appeding result to all previous search results
-        self.results = self.lastSearch + self.results
-        return self.lastSearch
+        self.results = modifiedResult + self.results
+                    
+        return modifiedResult
     
     # Retrueve info about specific video(s)
     def videos(self, id, fields=False):
         searchResult = self.youtube.videos().list(
             part='snippet,statistics',
-            fields='items(snippet(tags),statistics)' if fields else '*',
+            fields='items(snippet(description,tags),statistics)' if fields else '*',
             id=id
         ).execute()
         
