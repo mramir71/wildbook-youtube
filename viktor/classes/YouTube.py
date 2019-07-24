eeb1898d514877747873a67d815e052fb87bf70f
Viktor Kirillov <mr.kirillov@icloud.com>
2019-07-23 20:59:25 -0500

    Added multi-page result quering

--- viktor/classes/YouTube.py
+++ viktor/classes/YouTube.py
@@ -10,11 +10,9 @@ class YouTube:
         self._KEY = KEY
         self.youtube = build(self._YOUTUBE_API_SERVICE_NAME, self._YOUTUBE_API_VERSION, 
                              developerKey=self._KEY)
-        
-        self.lastSearch = None
-        self.results = []
-        
         self.db = db
+        self.nextPageToken = None
+        self.page = 1
     
     # Uses youtube.search() API method
     def search(self, q, limit=1, fields=False, save=False):
@@ -27,86 +25,96 @@ class YouTube:
         if (save and not self.db):
             save = False
             print("Please provide 'db' argument with an instance to database to save video(s).")
+            
+        # Going through pages in result
+        self.results = []
+        while(limit > 0):
+            print("Working with page", self.page)
+            
+            # Quering the result
+            searchResult = self.youtube.search().list(
+                q=q,
+                part='snippet',
+                fields='nextPageToken,pageInfo(totalResults),items(id,snippet(publishedAt,channelId,title,description))' if fields else '*',
+#                 fields='nextPageToken,items(id)' if fields else '*',
+                type='video',
+                maxResults=50 if limit>50 else limit,
+                pageToken=self.nextPageToken if self.nextPageToken else ''
+            ).execute()
+            items = searchResult['items']
+            
+            self.nextPageToken = searchResult['nextPageToken']
+            self.page += 1
+            limit -= 50
+
+            # Going through each result
+            modifiedResult = []
+            for item in items:
+
+                # Quering more details for this result
+                details = self.videos(item['id']['videoId'], fields=True)
+                for prop in details[0]:
+                    try:
+                        item[prop].update(details[0][prop])
+                    except KeyError:
+                        item[prop] = details[0][prop]
+
+                # WILDBOOK FORMAT
+                # try:
+                newItem = {
+                    "videoID": item['id']['videoId'],
+                    "title": {
+                        "original": item['snippet'].get('title', None),
+                        "eng": None, #[Microsoft translate]
+                    },
+                    "tags": {
+                        "original": item['snippet'].get('tags', []),
+                        "eng": [], #[Microsoft translate]
+                    },
+                    "description": {
+                        "original": item['snippet'].get('description', None),
+                        "eng": None, #[Microsoft translate]
+                    },
+                    "OCR": {
+                        "original": [], #[Azure]
+                        "eng": [], #[Azure, Microsoft translate]
+                    },
+                    "url": 'https://youtu.be/' + item['id']['videoId'],
+                    "animalsID": [], #[Wildbook]
+                    "curationStatus": None, #[Wildbook]
+                    "curationDecision": None, #[Wildbook]
+                    "publishedAt": item['snippet'].get('publishedAt', None),
+                    "uploadedAt": None, #[YouTube]
+                    "duration": None, #[YouTube]
+                    "regionRestriction": None, #[YouTube]
+                    "viewCount": item['statistics'].get('viewCount', None),
+                    "likeCount": item['statistics'].get('likeCount', None),
+                    "dislikeCount": item['statistics'].get('dislikeCount', None),
+                    "recordingDetails": {
+                        "location": None, #[YouTube]
+                        "date": None, #[YouTube]
+                    },
+                    "encounter": {
+                        "locationIDs": [], #[Wildbook]
+                        "dates": [], #[Wildbook]
+                    },
+                    "fileDetails": None, #[YouTube]
+                }
+                # except:
+                #     print(item)
+
+                # Saving item in database
+                if (save):
+                    self.db.addVideo(item['id']['videoId'], newItem)
+
+                modifiedResult.append(newItem)
+            
+            # Appeding result to all previous search results
+            self.results += modifiedResult
+
+        return self.results
+    
         
-        # Quering the result
-        searchResult = self.youtube.search().list(
-            q=q,
-            part='snippet',
-            fields='items(id,snippet(publishedAt,channelId,title,description))' if fields else '*',
-            type='video',
-            maxResults=limit
-        ).execute()
-
-        # Saving original YouTube responce for last search
-        self.lastSearch = searchResult['items']
-                
-        modifiedResult = []
-
-        # Going through each result
-        for item in self.lastSearch:
-
-            # Quering more details for this result
-            details = self.videos(item['id']['videoId'], fields=True)
-            for prop in details[0]:
-                try:
-                    item[prop].update(details[0][prop])
-                except KeyError:
-                    item[prop] = details[0][prop]
-
-            # WILDBOOK FORMAT
-            # try:
-            newItem = {
-                "videoID": item['id']['videoId'],
-                "title": {
-                    "original": item['snippet']['title'],
-                    "eng": None, #[Microsoft translate]
-                },
-                "tags": {
-                    "original": item['snippet'].get('tags', []),
-                    "eng": [], #[Microsoft translate]
-                },
-                "description": {
-                    "original": item['snippet']['description'],
-                    "eng": None, #[Microsoft translate]
-                },
-                "OCR": {
-                    "original": [], #[Azure]
-                    "eng": [], #[Azure, Microsoft translate]
-                },
-                "url": 'https://youtu.be/' + item['id']['videoId'],
-                "animalsID": [], #[Wildbook]
-                "curationStatus": None, #[Wildbook]
-                "curationDecision": None, #[Wildbook]
-                "publishedAt": item['snippet']['publishedAt'],
-                "uploadedAt": None, #[YouTube]
-                "duration": None, #[YouTube]
-                "regionRestriction": None, #[YouTube]
-                "viewCount": item['statistics']['viewCount'],
-                "likeCount": item['statistics']['likeCount'],
-                "dislikeCount": item['statistics']['dislikeCount'],
-                "recordingDetails": {
-                    "location": None, #[YouTube]
-                    "date": None, #[YouTube]
-                },
-                "encounter": {
-                    "locationIDs": [], #[Wildbook]
-                    "dates": [], #[Wildbook]
-                },
-                "fileDetails": None, #[YouTube]
-            }
-            # except:
-            #     print(item)
-
-            # Saving item in database
-            if (save):
-                self.db.addVideo(item['id']['videoId'], newItem)
-
-            modifiedResult.append(newItem)
-
-        # Appeding result to all previous search results
-        self.results = modifiedResult + self.results
-                    
-        return modifiedResult
     
     # Retrueve info about specific video(s)
     def videos(self, id, fields=False):
@@ -115,7 +123,7 @@ class YouTube:
             fields='items(snippet(description,tags),statistics)' if fields else '*',
             id=id
         ).execute()
-        
+
         return searchResult['items']
     
     # Retrieve info about channel
